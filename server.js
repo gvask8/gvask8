4ee9510912def9d37857067f161fc80743dd1679
gvask8 <daniel.geneve@hotmail.com>
2026-08-29 15:44:29 +0200

    Add route to serve index.html file

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const PORT = Number(process.env.PORT || 8080);
 app.use(express.json({ limit: '1mb' }));
 app.use(express.static('public'));
 
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(process.cwd(), 'index.html'));
+});
+
 function normalizeText(value) {
   return String(value ?? '')
     .normalize('NFD').replace(/[\u0300-\u036f]/g, '')
